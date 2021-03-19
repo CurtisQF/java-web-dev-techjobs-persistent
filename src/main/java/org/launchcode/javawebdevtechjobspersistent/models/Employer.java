36890ebd4d912bd08858caf482af607a2c032e37
curtismoeller <curtisqfarrell@gmail.com>
2021-03-18 19:57:30 -0500

    Completed Step 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location cannot be blank")
+    private String location;
 
+
+    public Employer (){};
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
